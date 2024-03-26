a2c23d96c04512300ecbe34f8acf12c4fab826f1
hyun3936 <hyun3936@naver.com>
2024-03-26 22:46:22 +0900

    03.26

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ app = Flask(__name__)
 
 # 날짜 형식 조정 ---------------------------------------------------------------------------------------------------------------
 
-def convert_timestamp_to_date(timestamp):
-    """밀리초 단위의 타임스탬프를 'Asia/Seoul' 시간대를 기준으로 'YYYY-MM-DD HH:MM:SS' 형태의 문자열로 변환합니다."""
-    dt_local = datetime.fromtimestamp(int(timestamp) / 1000, tz=ZoneInfo('Asia/Seoul'))
-    return dt_local.strftime('%Y-%m-%d %H:%M:%S')
+# def convert_timestamp_to_date(timestamp):
+#     """밀리초 단위의 타임스탬프를 'Asia/Seoul' 시간대를 기준으로 'YYYY-MM-DD HH:MM:SS' 형태의 문자열로 변환합니다."""
+#     dt_local = datetime.fromtimestamp(int(timestamp) / 1000, tz=ZoneInfo('Asia/Seoul'))
+#     return dt_local.strftime('%Y-%m-%d %H:%M:%S')
 
 # 결과 내보내기 ---------------------------------------------------------------------------------------------------------------
 
@@ -38,24 +38,8 @@ def handle_post_log(usercode):
     data = request.get_json(force=True)
       
     df = pd.DataFrame(data)
-
-    
-    # 'RegisterDate' 열에 있는 타임스탬프를 지정된 형식의 문자열로 변환
-    # df['RegisterDate'] = df['RegisterDate'].apply(convert_timestamp_to_date)
-
-    df['regidate'] = pd.to_datetime(df['regidate'])
-    df.sort_values(by='regidate', inplace=True)
-    df.fillna(method='ffill', inplace=True)
-    
-    # # 'registerDate' 컬럼이 있다면, 해당 컬럼의 타임스탬프를 날짜 형식으로 변환
-    # if 'registerDate' in df.columns:
-    #     df['registerDate'] = df['registerDate'].apply(lambda x: convert_timestamp_to_date(x))
-    
-    
     
     df = df.tail(10)  # 최근 10개 데이터만 사용
-    
-
     
     #  파일 이름 정의
     file_name = f"dataSaveTest_{usercode}.csv"  # 예를 들어 {usercode}, REST API로부터 받은 값
@@ -72,17 +56,31 @@ def handle_post_log(usercode):
     
     # 파일을 불러옵니다.
     df = pd.read_csv(file_name)
-
+    df = df.tail(10)  # 최근 10개 데이터만 사용
 
     # 심박수 데이터 추출 및 스케일링
     heartbeat_data = df['heartbeat'].values.reshape(-1, 1)
     scaler = MinMaxScaler(feature_range=(0, 1))
     heartbeat_data_scaled = scaler.fit_transform(heartbeat_data)
+    
+    
+    
+    
+    #     # 고정된 범위를 사용하여 MinMaxScaler 초기화
+    # scaler = MinMaxScaler(feature_range=(0, 1))
+    #     # 고정된 최소값과 최대값 설정
+    # scaler.data_min_ = np.array([40])  # 심박수 데이터의 최소값
+    # scaler.data_max_ = np.array([200])  # 심박수 데이터의 최대값
+    # scaler.scale_ = 1 / (scaler.data_max_ - scaler.data_min_)
+    # scaler.min_ = -scaler.data_min_ * scaler.scale_
+    # heartbeat_data_scaled = scaler.transform(heartbeat_data)
+    
+    
 
     # PyTorch 텐서로 변환 및 차원 조정
     X = torch.Tensor(heartbeat_data_scaled).unsqueeze(2)  # [1, 10, 1] 형태로 변환
 
-    # LSTM 기반 오토인코더 모델 정의 (변경 없음)
+    # LSTM 기반 오토인코더 모델 정의 
     class Autoencoder(nn.Module):
         def __init__(self, input_dim, hidden_dim, num_layers):
             super(Autoencoder, self).__init__()
@@ -111,14 +109,13 @@ def handle_post_log(usercode):
     
     # model.load_state_dict(torch.load(model_path))
     model.eval()
-    
-    
 
 
     # 데이터에 대한 예측 수행 및 재구성 오차 계산
     criterion = torch.nn.MSELoss()  # 평균 제곱 오차 손실 함수
     with torch.no_grad():
         outputs = model(X)
+         # 전체 배치에 대한 평균 재구성 오차를 계산합니다.
         reconstruction_errors = criterion(outputs, X).item()
 
     print(f'재구성 오차: {reconstruction_errors}')
@@ -132,6 +129,7 @@ def handle_post_log(usercode):
 
     with torch.no_grad():
         outputs = model(X)
+        # 각 데이터 포인트별 재구성 오차를 계산합니다.
         for i in range(X.size(0)):
             loss = criterion(outputs[i], X[i]).item()
             reconstruction_errors.append(loss)
@@ -139,126 +137,50 @@ def handle_post_log(usercode):
     # 임계값 설정 (이전에 정의한 방식을 그대로 사용)
     mean_reconstruction_error = np.mean(reconstruction_errors)
     std_reconstruction_error = np.std(reconstruction_errors)
-    threshold = mean_reconstruction_error + 3 * std_reconstruction_error
+    threshold = mean_reconstruction_error + 1 * std_reconstruction_error
 
-    # # 재구성 오차 시각화
-    # plt.hist(reconstruction_errors, bins=50, alpha=0.7, color='blue', label='Reconstruction errors')
-    # plt.axvline(threshold, color='red', linestyle='dashed', linewidth=2, label='Threshold')
-    # plt.title('Histogram of Reconstruction Errors')
-    # plt.xlabel('Reconstruction error')
-    # plt.ylabel('Frequency')
-    # plt.legend()
-    # plt.show()
+    # 상태 값에 대한 명시적 정의
+    SAFE = 0
+    CAUTION = 1
+    WARNING = 2
+    check = 9
 
-    # # 재구성 오차를 기반으로 이상치를 식별합니다.
-    # anomalies = [i for i, error in enumerate(reconstruction_errors) if error > threshold]
+    # 이상치를 식별합니다.
+    anomalies = [i for i, error in enumerate(reconstruction_errors) if error > threshold]
 
-    # # 결과 DataFrame 초기화
-    # results = pd.DataFrame({
-    #     'status': [1] * len(df),  # 초기 상태를 'SAFE'로 설정
-    #     'worker_id': df['usercode'].values  # 'UserCode'를 'worker_id'로 사용
-    # })
+    # 결과 DataFrame 초기화
+    results = pd.DataFrame({
+        'status': [SAFE] * len(df),  # 모든 초기 상태를 'SAFE'로 설정
+        'worker_id': df['usercode'].values  # 'UserCode'를 'worker_id'로 사용
+    })
 
-    
-    # SAFE : 1
-    # CAUTION : 2
+    # 이상치에 대한 상태를 'check'으로 업데이트
+    for idx in anomalies:
+        results.at[idx, 'status'] = check
 
+    # 'status' 열에서 CAUTION(2)이 3개 이상 있는지 검사
+    caution_count = sum(results['status'] == check)
 
-    # # 이상치에 대한 상태를 'CAUTION'으로 업데이트
-    # for idx in anomalies:
-    #     if idx < len(results):  # 인덱스가 결과 DataFrame의 범위 내에 있는지 확인
-    #         results.at[idx, 'status'] = 2
-            
-    # # print(results)
+    if caution_count > 2: # 3개 이상
+        overall_status = WARNING
+    elif caution_count > 1: # 2개 이상
+        overall_status = CAUTION
+    else:                   # 1개 이하
+        overall_status = SAFE
 
+    # JSON 형식으로 결과 반환
+    result = {
+        'status': overall_status,
+        'worker_id': int(df['usercode'].iloc[-1])  # 마지막 worker_id 반환
+    }
 
-    # # 'status' 열에서 'CAUTION'이 하나라도 있는지 검사
-    # if 2 in results['status'].values:
-    #     overall_status = 2
-    # else:
-    #     overall_status = 1
+    # 결과 출력 (Flask 등의 웹 프레임워크 사용 시)
+    print(f"results : \n{results}")
+    print(f"result : {result}")
+        
+    print(f"threshold : {threshold}")
 
-    # # JSON 형식으로 결과 반환
-    # result = {
-    #     'status': overall_status,
-    #     'worker_id': int(df['usercode'].values[0])  # 리스트로 감싸 단일 값으로 설정
-    # }
-
-    # # print(result)
-
-    # return jsonify(result)
-    
-    
-        # 두번째------------------------------------------------------------------------------------------------
-    
-    # # 상태 값에 대한 명시적 정의
-    # SAFE = 1
-    # CAUTION = 2
-
-    # # 이상치를 식별합니다.
-    # anomalies = [i for i, error in enumerate(reconstruction_errors) if error > threshold]
-
-    # # 결과 DataFrame 초기화
-    # results = pd.DataFrame({
-    #     'status': SAFE,  # 초기 상태를 'SAFE'로 설정
-    #     'worker_id': df['usercode']  # 'UserCode'를 'worker_id'로 사용
-    # })
-
-    # # 이상치에 대한 상태를 'CAUTION'으로 업데이트
-    # for idx in anomalies:
-    #     # if idx < len(results):  # 인덱스가 결과 DataFrame의 범위 내에 있는지 확인
-    #     results.at[idx, 'status'] = CAUTION
-
-    # # 'status' 열에서 CAUTION(2)이 하나라도 있는지 검사
-    # overall_status = CAUTION if CAUTION in results['status'].values else SAFE
-
-    # # JSON 형식으로 결과 반환
-    # result = {
-    #     'status': overall_status,
-    #     'worker_id': int(df['usercode'].iloc[-1])  # 마지막 worker_id 반환, 모든 worker_id를 반환하는 것이 의도된 경우 수정 필요
-    # }
-
-    # # 결과 출력 (Flask 등의 웹 프레임워크 사용 시)
-    # return jsonify(result)
-
-
-        # 세번째 --------------------------------------------------------------------------------------------------------------------------
-
-    # # 상태 값에 대한 명시적 정의
-    # SAFE = 1
-    # CAUTION = 2
-
-    # # 이상치를 식별합니다.
-    # anomalies = [i for i, error in enumerate(reconstruction_errors) if error > threshold]
-
-    # # 결과 DataFrame 초기화
-    # results = pd.DataFrame({
-    #     'status': [SAFE] * len(df),  # 모든 초기 상태를 'SAFE'로 설정
-    #     'worker_id': df['usercode']  # 'UserCode'를 'worker_id'로 사용
-    # })
-
-    # # 이상치에 대한 상태를 'CAUTION'으로 업데이트
-    # for idx in anomalies:
-    #     results.at[idx, 'status'] = CAUTION
-
-    # # 'status' 열에서 CAUTION(2)이 3개 이상 있는지 검사
-    # caution_count = sum(results['status'] == CAUTION)
-
-    # if caution_count > 9:
-    #     overall_status = CAUTION
-    # else:
-    #     overall_status = SAFE
-
-    # # JSON 형식으로 결과 반환
-    # result = {
-    #     'status': overall_status,
-    #     'worker_id': int(df['usercode'].iloc[-1])  # 마지막 worker_id 반환
-    # }
-
-    # # 결과 출력 (Flask 등의 웹 프레임워크 사용 시)
-    # print(f"result : {result}")
-    # print(f"results : {results}")
-    # return jsonify(result)
+    return jsonify(result)
     
     
 
